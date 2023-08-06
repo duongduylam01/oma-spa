f401db62f810847a328089877d65bf9de9076cb1
Lam <duongduylam01@gmail.com>
2023-08-06 21:39:08 +0700

    OMA-05 Update header

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,7 +9,36 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
+  selectCase: any = {
+    HEADER_MENU: 'headerMenu',
+  };
+  headerLogo = 'assets/img/logo_1.png';
+  headerMenuList: any = [
+    {id: 'homepage', name: 'Homepage', isActive: true},
+    {id: 'service', name: 'Service', isActive: false},
+    {id: 'benefit', name: 'Benefit', isActive: false},
+    {id: 'gallery', name: 'Gallery', isActive: false},
+    {id: 'testimonials', name: 'Testimonials', isActive: false},
+    {id: 'contact', name: 'Contact', isActive: false},
+  ];
+  headerLanguageList: any = [
+    {name: 'Tiếng việt', icon: 'assets/img/vn-flag-1.png'},
+    {name: 'English', icon: 'assets/img/vn-flag-1.png'}
+  ];
+
   ngOnInit(): void {
+
   }
 
+  onSelect(selectCase: any, i: any) {
+    switch (selectCase) {
+      case this.selectCase.HEADER_MENU: {
+        if (i >= 0 && this.headerMenuList[i]) {
+          this.headerMenuList.forEach((d:any) => d.isActive = false);
+          this.headerMenuList[i].isActive = true;
+        }
+        break;
+      }
+    }
+  }
 }
